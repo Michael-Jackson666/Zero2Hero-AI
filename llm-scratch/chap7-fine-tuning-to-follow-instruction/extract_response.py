08c19654efb50ca598e2a2f3e9b0c3876d796653
Jack <jackson11235813@163.com>
2025-09-15 21:15:54 +0800

    track some requirements

--- llm-scratch/chap7-fine-tuning-to-follow-instruction/extract_response.py
+++ llm-scratch/chap7-fine-tuning-to-follow-instruction/extract_response.py
@@ -119,16 +119,9 @@ def custom_collate_fn(
 
     return inputs_tensor, targets_tensor
 
-# new in this file
+# set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-elif torch.backends.mps.is_available():
-    device = torch.device("mps")
-else:
-    device = torch.device("cpu")
 
-# print("device:", device)
 from functools import partial
 custom_collate_fn = partial(
     custom_collate_fn,
@@ -172,6 +165,132 @@ test_loader = DataLoader(
     num_workers=num_workers
 )
 
-# print("Train loader:")
-# for inputs, targets in train_loader:
-#     print(inputs.shape, targets.shape)
+# Loading the pretrained model
+from gpt_download import download_and_load_gpt2, load_gpt2_params_from_tf_ckpt
+from previous_chapters import GPTModel, load_weights_into_gpt
+import tensorflow as tf
+
+BASE_CONFIG = {
+    "vocab_size": 50257,     # Vocabulary size
+    "context_length": 1024,  # Context length
+    "drop_rate": 0.0,        # Dropout rate
+    "qkv_bias": True         # Query-key-value bias
+}
+
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+CHOOSE_MODEL = "gpt2-medium (355M)"
+
+BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+# download and load gpt2 weights
+# settings, params = download_and_load_gpt2(
+#     model_size=model_size,
+#     models_dir="gpt2"
+# )
+
+# only load weights into our model
+model_size = "355M"
+models_dir = "gpt2"
+model_dir = os.path.join(models_dir, model_size)
+
+settings_path = os.path.join(model_dir, "hparams.json")
+with open(settings_path, "r", encoding="utf-8") as f:
+    settings = json.load(f)
+
+ckpt_path = tf.train.latest_checkpoint(model_dir)
+params = load_gpt2_params_from_tf_ckpt(ckpt_path, settings)
+
+# set model
+model = GPTModel(BASE_CONFIG)
+load_weights_into_gpt(model, params)
+model.eval()
+
+from previous_chapters import (
+    calc_loss_loader,
+    train_model_simple
+)
+
+# Instruction fine-tuning the pretrained LLM
+import time
+
+start_time = time.time()
+torch.manual_seed(123)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.00005, weight_decay=0.1)
+num_epochs = 2
+
+train_losses, val_losses, tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device,
+    num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+    start_context=format_input(val_data[0]), tokenizer=tokenizer
+)
+
+end_time = time.time()
+execution_time_minutes = (end_time - start_time) / 60
+print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+
+# plot losses
+from previous_chapters import plot_losses
+epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+# save model
+model_path = "fine_tuned_instructions.pth"
+torch.save(model.state_dict(), model_path)
+
+# new in this file
+# Extracting and displaying model responses
+from previous_chapters import generate, text_to_token_ids, token_ids_to_text
+torch.manual_seed(123)
+
+
+for entry in test_data[:3]:
+
+    input_text = format_input(entry)
+
+    token_ids = generate(
+        model=model,
+        idx=text_to_token_ids(input_text, tokenizer).to(device),
+        max_new_tokens=256,
+        context_size=BASE_CONFIG["context_length"],
+        eos_id=50256
+    )
+    generated_text = token_ids_to_text(token_ids, tokenizer)
+    response_text = (
+        generated_text[len(input_text):]
+        .replace("### Response:", "")
+        .strip()
+)
+
+    print(input_text)
+    print(f"\nCorrect response:\n>> {entry['output']}")
+    print(f"\nModel response:\n>> {response_text.strip()}")
+    print("-------------------------------------")
+
+from tqdm import tqdm
+
+for i, entry in tqdm(enumerate(test_data), total=len(test_data)):
+
+    input_text = format_input(entry)
+
+    token_ids = generate(
+        model=model,
+        idx=text_to_token_ids(input_text, tokenizer).to(device),
+        max_new_tokens=256,
+        context_size=BASE_CONFIG["context_length"],
+        eos_id=50256
+    )
+    generated_text = token_ids_to_text(token_ids, tokenizer)
+    response_text = generated_text[len(input_text):].replace("### Response:", "").strip()
+
+    test_data[i]["model_response"] = response_text
+
+
+with open("instruction-data-with-response.json", "w") as file:
+    json.dump(test_data, file, indent=4)  # "indent" for pretty-printing
