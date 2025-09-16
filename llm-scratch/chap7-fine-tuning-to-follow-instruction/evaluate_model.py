65a7bb7f9efdb04837f7b9640382b3e6865a9f85
Jack <jackson11235813@163.com>
2025-09-16 10:26:14 +0800

    evaluate the finetuning model by Llama3

---
+++ llm-scratch/chap7-fine-tuning-to-follow-instruction/evaluate_model.py
@@ -0,0 +1,126 @@
+import psutil
+
+# run ollama server before executing this script
+def check_if_running(process_name):
+    running = False
+    for proc in psutil.process_iter(["name"]):
+        if process_name in proc.info["name"]:
+            running = True
+            break
+    return running
+
+ollama_running = check_if_running("ollama")
+
+if not ollama_running:
+    raise RuntimeError("Ollama not running. Launch ollama before proceeding.")
+print("Ollama running:", check_if_running("ollama"))
+
+import json
+from tqdm import tqdm
+
+file_path = "instruction-data-with-response_gpt2-medium_355M.json"
+
+with open(file_path, "r") as file:
+    test_data = json.load(file)
+
+def format_input(entry):
+    instruction_text = (
+        f"Below is an instruction that describes a task. "
+        f"Write a response that appropriately completes the request."
+        f"\n\n### Instruction:\n{entry['instruction']}"
+    )
+
+    input_text = f"\n\n### Input:\n{entry['input']}" if entry["input"] else ""
+
+    return instruction_text + input_text
+
+# Querying a local Ollama model
+import urllib.request
+
+def query_model(
+    prompt,
+    model="llama3",
+    url="http://localhost:11434/api/chat"
+):
+    # Create the data payload as a dictionary
+    data = {
+        "model": model,
+        "messages": [
+            {"role": "user", "content": prompt}
+        ],
+        "options": {     # Settings below are required for deterministic responses
+            "seed": 123,
+            "temperature": 0,
+            "num_ctx": 2048
+        }
+    }
+
+    # Convert the dictionary to a JSON formatted string and encode it to bytes
+    payload = json.dumps(data).encode("utf-8")
+
+    # Create a request object, setting the method to POST and adding necessary headers
+    request = urllib.request.Request(
+        url,
+        data=payload,
+        method="POST"
+    )
+    request.add_header("Content-Type", "application/json")
+
+    # Send the request and capture the response
+    response_data = ""
+    with urllib.request.urlopen(request) as response:
+        # Read and decode the response
+        while True:
+            line = response.readline().decode("utf-8")
+            if not line:
+                break
+            response_json = json.loads(line)
+            response_data += response_json["message"]["content"]
+
+    return response_data
+
+# test a simple query
+# model = "llama3"
+# result = query_model("What do Llamas eat?", model)
+# print(result)
+
+
+# evaluating the model on the test set
+# for entry in test_data[:3]:
+#     prompt = (
+#         f"Given the input `{format_input(entry)}` "
+#         f"and correct output `{entry['output']}`, "
+#         f"score the model response `{entry['model_response']}`"
+#         f" on a scale from 0 to 100, where 100 is the best score. "
+#     )
+#     print("\nDataset response:")
+#     print(">>", entry['output'])
+#     print("\nModel response:")
+#     print(">>", entry["model_response"])
+#     print("\nScore:")
+#     print(">>", query_model(prompt))
+#     print("\n-------------------------")
+
+# score the entire test set
+def generate_model_scores(json_data, json_key, model="llama3"):
+    scores = []
+    for entry in tqdm(json_data, desc="Scoring entries"):
+        prompt = (
+            f"Given the input `{format_input(entry)}` "
+            f"and correct output `{entry['output']}`, "
+            f"score the model response `{entry[json_key]}`"
+            f" on a scale from 0 to 100, where 100 is the best score. "
+            f"Respond with the integer number only."
+        )
+        score = query_model(prompt, model)
+        try:
+            scores.append(int(score))
+        except ValueError:
+            print(f"Could not convert score: {score}")
+            continue
+
+    return scores
+
+scores = generate_model_scores(test_data, "model_response")
+print(f"Number of scores: {len(scores)} of {len(test_data)}")
+print(f"Average score: {sum(scores)/len(scores):.2f}\n")
